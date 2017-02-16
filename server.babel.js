842fa84416cd6c4dc52796e883697f51fa0c501d
bud aminof <budaminof@gmail.com>
2017-02-16 12:10:23 -0700

    test: checking what on the express side will break when adding.
    do I need to add path?

--- server.babel.js
+++ server.babel.js
@@ -17,9 +17,7 @@ app.listen(process.env.PORT || 3000);
 //
 // // import routes from './routes/index';
 // // import users from './routes/users';
-//
-// const app = express();
-//
+
 // // view engine setup
 // // app.set('views', path.join(__dirname, 'views'));
 // // app.set('view engine', 'jade');
@@ -28,45 +26,47 @@ app.listen(process.env.PORT || 3000);
 // app.use(bodyParser.json());
 // app.use(bodyParser.urlencoded({ extended: false }));
 // app.use(cookieParser());
+
+// need to require path
 // app.use(express.static(path.join(__dirname, 'public')));
 //
 // // app.use('/', routes);
 // // app.use('/users', users);
 //
-// app.use('/*', function (req, res, next) {
-//   res.sendFile('index.html', {root: __dirname + '/public/'});
-// })
-//
-// // catch 404 and forward to error handler
-// app.use(function(req, res, next) {
-//   var err = new Error('Not Found');
-//   err.status = 404;
-//   next(err);
-// });
-//
-// // error handlers
-//
-// // development error handler
-// // will print stacktrace
-// if (app.get('env') === 'development') {
-//   app.use(function(err, req, res, next) {
-//     res.status(err.status || 500);
-//     res.json('error', {
-//       message: err.message,
-//       error: err
-//     });
-//   });
-// }
-//
-// // production error handler
-// // no stacktraces leaked to user
-// app.use(function(err, req, res, next) {
-//   res.status(err.status || 500);
-//   res.json('error', {
-//     message: err.message,
-//     error: {}
-//   });
-// });
-//
-//
-// module.exports = app;
+app.use('/*', function (req, res, next) {
+  res.sendFile('index.html', {root: __dirname + '/public/'});
+})
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+// error handlers
+
+// development error handler
+// will print stacktrace
+if (app.get('env') === 'development') {
+  app.use(function(err, req, res, next) {
+    res.status(err.status || 500);
+    res.json('error', {
+      message: err.message,
+      error: err
+    });
+  });
+}
+
+// production error handler
+// no stacktraces leaked to user
+app.use(function(err, req, res, next) {
+  res.status(err.status || 500);
+  res.json('error', {
+    message: err.message,
+    error: {}
+  });
+});
+
+
+module.exports = app;
